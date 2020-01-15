1d33f286704f3c153e910590966987abd17d538a
rania <raniaa13.rh@gmail.com>
2020-01-15 11:51:22 -0600

    tests

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React , {Component}  from 'react';
+import './Button.css';
+
+
+class Button extends Component {
+    // isOpreator = val => {
+    //     return !isNaN(val) || val==="." || val==="=";
+    // };
+
+  render() {
+     
+     return (
+     <div 
+       className= "button "
+       onClick= {() => this.props.handleClick(this.props.children)}
+       >
+            {this.props.children}
+     </div>
+     )
+  }  
+}
+
+export default Button;
---
+++ src/components/Input.js
@@ -0,0 +1,16 @@
+import React , {Component}  from 'react';
+import './Input.css';
+
+
+class Input extends Component {
+    render() {
+     
+     return (
+     <div className= "input ">
+            {this.props.children}
+     </div>
+     )
+  }  
+}
+
+export default Input;
